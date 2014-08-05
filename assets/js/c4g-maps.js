2ec6b54e33c4a5665abcd038f3a2d02561a5f0c4
TDo <tobias.dobbrunz@kuestenschmiede.de>
2014-08-05 17:22:21 +0200

    load basic map-options
    adds the functionality to load map-zoom/-position from the DB.
    otherwise it loads with "defaults"

--- assets/js/c4g-maps.js
+++ assets/js/c4g-maps.js
@@ -3,13 +3,30 @@ var c4g = {};
 
 /**
  * [Map description]
- * @param {[type]} config [description]
+ * @param {[type]} mapData [description]
  */
-c4g.Map = function(config) 
+c4g.Map = function(mapData) 
 {
     var self = this;
-
     self = {};
+
+    //---
+    this.map = null;
+    	// this.baseLayers = null;
+    	// this.layers = null;
+    this.controls = null;
+    //---
+
+    mapData = $.extend({
+			// restUrl : 'api',
+			mapDiv : 'c4gMap_' + mapData.id,
+			// width : '',
+			// height : '',
+			center_lat : 37.41,
+			center_lon : 8.82,
+			zoom : 4,
+			calc_extent: 'CENTERZOOM'
+		}, mapData);
 
     //[ NOTES ] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // self.closeAll = function() {};
@@ -21,26 +38,24 @@ c4g.Map = function(config)
     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     //[ DEV ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    console.log('c4g-maps loaded');
+    console.log( mapData );
 
-    if (typeof jQuery != 'undefined') {
-    	console.log('jQuery loaded');
-    }
-    if (typeof ol != 'undefined') {
-    	console.log('OpenLayers 3 loaded');
-    }
-
-    var map = new ol.Map({
-	    target: config.mapDiv,
+    this.map = new ol.Map({
+	    target: mapData.mapDiv,
 	    layers: [
-	      new ol.layer.Tile({
-	        source: new ol.source.MapQuest({layer: 'sat'})
-	      })
+		    new ol.layer.Tile({
+		      	source: new ol.source.OSM()
+		    })
 	    ],
 	    view: new ol.View({
-	      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-	      zoom: 4
+	    	// projection: ol.proj.get('EPSG:4326'),
+	    	// center: [parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)],
+	    	center: ol.proj.transform([parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)], 'EPSG:4326', 'EPSG:3857'),
+	    	zoom: parseInt(mapData.zoom)
 	    })
   	});
+
+
+  	this.map.addControl(new ol.control.MousePosition({projection:'EPSG:4326'}));
     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 };
--- classes/C4GMaps.php
+++ classes/C4GMaps.php
@@ -84,7 +84,7 @@ class C4GMaps
 
 		// get map
 		$map = C4gMapsModel::findByPk( $objThis->c4g_map_id );
-		// if(empty( $map )) return false;
+		if(!$map) return false;
 		$mapData['id'] = $objThis->c4g_map_id;
 
 		// --------------------------------------------------------------------
@@ -122,6 +122,66 @@ class C4GMaps
         }
 		$mapData['profile'] = $profileId;
 
+		if ($map->id == 0) {
+			// use default settings
+			// $mapData['center_lon'] = '1.0';
+			// $mapData['center_lat'] = '1.0';
+			$mapDat ['zoom'] = $objThis->c4g_map_zoom;
+			if ($mapData['zoom'] == 0)
+				$mapData['zoom'] = 1;
+			$mapsize = deserialize( $objThis->c4g_map_mapsize );
+			if (! is_array ( $mapsize ) || ($mapsize [0] == 0)) {
+				$mapData['width'] = '400px';
+				$mapData['height'] = '300px';				
+			} else {
+				$mapData['width'] = $mapsize [0] . $mapsize [2];
+				$mapData['height'] = $mapsize [1] . $mapsize [2];
+			}	
+			// $mapData ['calc_extent'] = 'CENTERZOOM';
+		} else {
+			$mapData['center_lon'] = $map->center_geox;
+			$mapData['center_lat'] = $map->center_geoy;
+			if (is_numeric ( $objThis->c4g_map_zoom ) && ($objThis->c4g_map_zoom > 0)) {
+				$mapData['zoom'] = $objThis->c4g_map_zoom;
+			} else {
+				$mapData['zoom'] = $map->zoom;
+			}
+			if ($map->geolocation) {
+				$mapData['geolocation'] = true;
+				$mapData['geolocation_zoom'] = $map->geolocation_zoom;				
+			}
+			$mapsize = deserialize ( $objThis->c4g_map_mapsize );
+			if (! is_array ( $mapsize ) || ($mapsize [0] == 0)) {
+				$mapsize = deserialize ( $map->mapsize );
+			}
+			$mapData['width'] = $mapsize [0] . $mapsize [2];
+			$mapData['height'] = $mapsize [1] . $mapsize [2];
+            if ($map->auto_width) {
+                $mapData['auto_width'] = true;
+                $mapData['auto_width_gap'] = $map->auto_width_gap;
+                $mapData['auto_width_min'] = $map->auto_width_min;
+                $mapData['auto_width_max'] = $map->auto_width_max;
+            }
+            if ($map->auto_height) {
+                $mapData['auto_height'] = true;
+                $mapData['auto_height_gap'] = $map->auto_height_gap;
+                $mapData['auto_height_min'] = $map->auto_height_min;
+                $mapData['auto_height_max'] = $map->auto_height_max;
+            }
+    	
+			$mapData['calc_extent'] = $map->calc_extent;
+			if ($map->calc_extent=='LOCATIONS') {
+				$mapData['min_gap'] = $map->min_gap;				
+			}
+			
+	  		$mapData['restrict_area'] = $map->restrict_area;
+	  		if ($map->restrict_area) {  		    		    		  
+	  			$mapData['restr_bottomleft_lon'] = $map->restr_bottomleft_lon;
+	  			$mapData['restr_bottomleft_lat'] = $map->restr_bottomleft_lat;
+	  			$mapData['restr_topright_lon'] = $map->restr_topright_lon;
+	  			$mapData['restr_topright_lat'] = $map->restr_topright_lat;
+	  		}
+		}
 
 
 		//[ DEV ]!!!!!!!!!!!!!!!!!!!!!!!!
@@ -134,7 +194,7 @@ class C4GMaps
 		$GLOBALS['TL_CSS'][] = $GLOBALS['c4g_maps_extension']['css_openlayers']['DEFAULT'];
 
 
-		$mapData['mapDiv'] = 'c4gMap';
+		// $mapData['mapDiv'] = 'c4gMap';
 		// return print_r( $mapData, true );
 		return $mapData;
 
