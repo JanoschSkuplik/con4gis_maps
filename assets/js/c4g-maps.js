760d5990e57858de8decde485c7f76b1d9deaedf
TDo <tobias.dobbrunz@kuestenschmiede.de>
2014-08-11 15:08:35 +0200

    update OL (gamma3 -> gamma4)
    +minor JS-changes

--- assets/js/c4g-maps.js
+++ assets/js/c4g-maps.js
@@ -1,80 +1,86 @@
 // "namespace"
-var c4g = {};
+var c4g = c4g || {};
 
-/**
- * [Map description]
- * @param {json-object} mapData [object to configure con4gis-maps. 
- *                               See "docs/mapData-values.md" 
- *                               to get a list of valid values for this object]
- */
-c4g.Map = function(mapData) 
+(function($) 
 {
-    var self = this;
-    self = {};
+    "use strict";
 
-    //---
-    this.map = null;
-    	// this.baseLayers = null;
-    	// this.layers = null;
-    this.controls = null;
-    //---
+    /**
+     * [Map description]
+     * @param {json-object} mapData [object to configure con4gis-maps. 
+     *                               See "docs/mapData-values.md" 
+     *                               to get a list of valid values for this object]
+     */
+    c4g.Map = function(mapData) 
+    {
+        var self = this;
+        self = {};
 
-    mapData = $.extend({
-			// restUrl : 'api',
-            addIdToDiv : false, 
-            mapId : 1, 
-			mapDiv : 'c4g_Map',
-			center_lat : 37.41,
-			center_lon : 8.82,
-			zoom : 4,
-			calc_extent: 'CENTERZOOM'
-		}, mapData);
+        //---
+        this.map = null;
+            // this.baseLayers = null;
+            // this.layers = null;
+        this.controls = null;
+        //---
 
-    if (mapData.addIdToDiv) {
-        mapData.mapDiv += mapData.mapId;
-    }
-    
-    //[ NOTES ] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // self.closeAll = function() {};
+        mapData = $.extend({
+                // restUrl : 'api',
+                addIdToDiv : false, 
+                mapId : 1, 
+                mapDiv : 'c4g_Map',
+                center_lat : 37.41,
+                center_lon : 8.82,
+                zoom : 4,
+                calc_extent: 'CENTERZOOM'
+            }, mapData);
 
-    // self.isLoading = ...
+        if (mapData.addIdToDiv) {
+            mapData.mapDiv += mapData.mapId;
+        }
 
-    // foreach x in extensions
-    // x.init(self);
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //[ NOTES ] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // self.closeAll = function() {};
 
-    //[ DEV ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    console.log( mapData );
+        // self.isLoading = ...
 
-    this.map = new ol.Map({
-	    target: mapData.mapDiv,
-	    layers: [
-		    new ol.layer.Tile({
-		      	source: new ol.source.OSM()
-		    })
-	    ],
-	    view: new ol.View({
-	    	// projection: ol.proj.get('EPSG:4326'),
-	    	// center: [parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)],
-	    	center: ol.proj.transform([parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)], 'EPSG:4326', 'EPSG:3857'),
-	    	zoom: parseInt(mapData.zoom)
-	    })
-  	});
+        // foreach x in extensions
+        // x.init(self);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    // set map-size and -margin
-    if (mapData.width) {
-        document.getElementById(mapData.mapDiv).style.width = mapData.width;
-    }
-    if (mapData.height) {
-        document.getElementById(mapData.mapDiv).style.height = mapData.height;
-    }
-    if (mapData.margin) {
-        document.getElementById(mapData.mapDiv).style.margin = mapData.margin;
-    }
-    this.map.updateSize();
-    // ---
+        //[ DEV ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        console.log( mapData );
 
-  	this.map.addControl(new ol.control.MousePosition({projection:'EPSG:4326'}));
+        this.map = new ol.Map({
+            target: mapData.mapDiv,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            view: new ol.View({
+                // projection: ol.proj.get('EPSG:4326'),
+                // center: [parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)],
+                center: ol.proj.transform([parseFloat(mapData.center_lon), parseFloat(mapData.center_lat)], 'EPSG:4326', 'EPSG:3857'),
+                zoom: parseInt(mapData.zoom)
+            })
+        });
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-};
+        // set map-size and -margin
+        if (mapData.width) {
+            document.getElementById(mapData.mapDiv).style.width = mapData.width;
+        }
+        if (mapData.height) {
+            document.getElementById(mapData.mapDiv).style.height = mapData.height;
+        }
+        if (mapData.margin) {
+            document.getElementById(mapData.mapDiv).style.margin = mapData.margin;
+        }
+        this.map.updateSize();
+        // ---
+
+        this.map.addControl(new ol.control.MousePosition({projection:'EPSG:4326'}));
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    };
+
+})(jQuery);
