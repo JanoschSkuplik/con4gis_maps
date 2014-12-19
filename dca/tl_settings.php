881fdc5e308be4b54d334b603de8cc124a4dd900
coastforge-tdo <tobias.dobbrunz@kuestenschmiede.de>
2014-12-19 12:02:04 +0100

    Fix class-conflict with cfs_maps
    + autoload adjustment

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -9,11 +9,11 @@
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
  * @copyright Küstenschmiede GmbH Software & Design 2014
  * @link      https://www.kuestenschmiede.de
- * @filesource 
+ * @filesource
  */
 
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{c4g_maps_legend},disabledC4gMapObjects'; 
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{c4g_maps_legend},disabledC4gMapObjects';
 
 /**
  * Add fields
@@ -22,12 +22,12 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['disabledC4gMapObjects'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['disabledC4gMapObjects'],
 	'inputType'               => 'checkbox',
-	'options_callback'        => array('tl_settings_maps', 'getMapTables'),
+	'options_callback'        => array('tl_settings_con4gis_maps', 'getMapTables'),
 	'eval'                    => array('multiple'=>true)
 );
 
 /**
- * Class tl_settings_maps
+ * Class tl_settings_con4gis_maps
  *
  * Provide methods that are used by the data configuration array.
  * @copyright  Küstenschmiede GmbH Software & Design 2012
@@ -35,7 +35,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['disabledC4gMapObjects'] = array
  * @package    con4gis
  * @author     Jürgen Witte <http://www.kuestenschmiede.de>
  */
-class tl_settings_maps extends tl_settings
+class tl_settings_con4gis_maps extends tl_settings
 {
 	/**
 	 * Return available Map tables
@@ -45,7 +45,7 @@ class tl_settings_maps extends tl_settings
 	public function getMapTables()
 	{
 		$tables = array();
-		foreach ($GLOBALS['c4g_maps_extension']['sourcetable'] as $key=>$sourcetable)
+		foreach ($GLOBALS['con4gis_maps_extension']['sourcetable'] as $key=>$sourcetable)
 		{
 			$tables[$key] = $GLOBALS['TL_LANG']['c4g_maps']['sourcetable'][$key]['name'];
 		}
